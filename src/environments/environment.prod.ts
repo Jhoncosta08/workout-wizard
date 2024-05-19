1e826a41c286232bc398f0ad3f3e5656fc89a2e4
jhonatan <jhonatan.s.costa97@gmail.com>
2024-05-19 15:21:17 -0300

    feat(auth): start working in a welcome page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDUR5nWOeWmK_g_gKU2vN7rEttp8b4Uioc",
+    authDomain: "workout-wizard-app.firebaseapp.com",
+    projectId: "workout-wizard-app",
+    storageBucket: "workout-wizard-app.appspot.com",
+    messagingSenderId: "384472738830",
+    appId: "1:384472738830:web:a14ecc704a26a066e7e05f"
+  }
 };
