1a0de3003f56019a1be410d16a2edc8f38338920
jhonatan <jhonatan.s.costa97@gmail.com>
2024-05-19 13:13:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'workout-wizard',
+  webDir: 'www'
+};
+
+export default config;
