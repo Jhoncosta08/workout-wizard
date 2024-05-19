ffea16cd07ce5c6417f48aea93fd29a797420637
jhonatan <jhonatan.s.costa97@gmail.com>
2024-05-19 13:31:50 -0300

    feat(app): my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'create-account',
+    loadChildren: () => import('./pages/auth/create-account/create-account.module').then( m => m.CreateAccountPageModule)
   },
 ];
 
---
+++ src/app/components/footer-tab/footer-tab.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-tab',
+  templateUrl: './footer-tab.component.html',
+  styleUrls: ['./footer-tab.component.scss'],
+})
+export class FooterTabComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
