1aaff0386bcd8b827c83c4935c3dbd2c503a654a
jhonatan <jhonatan.s.costa97@gmail.com>
2024-05-19 13:45:52 -0300

    feat(app): added the firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDUR5nWOeWmK_g_gKU2vN7rEttp8b4Uioc",
+  authDomain: "workout-wizard-app.firebaseapp.com",
+  projectId: "workout-wizard-app",
+  storageBucket: "workout-wizard-app.appspot.com",
+  messagingSenderId: "384472738830",
+  appId: "1:384472738830:web:a14ecc704a26a066e7e05f"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
