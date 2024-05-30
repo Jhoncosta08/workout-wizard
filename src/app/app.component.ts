fc8644ae87ba08b90ab26e89fd74f5ec77d2417f
jhonatan <jhonatan.s.costa97@gmail.com>
2024-05-30 14:07:09 -0300

    fix(app): remove refresher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,9 @@ import {Router} from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  canRefresh: boolean = false;
 
 
   constructor(private router: Router) {}
-
-
-  onScroll(event: any): void {
-    const scrollTop = event.detail.scrollTop;
-    this.canRefresh = scrollTop === 0;
-  }
-
-
-  handleRefresh(event: any): void {
-    setTimeout((): void => {
-      window.location.reload();
-      event.target.complete();
-    }, 1000);
-  }
 
 
   showNavMenu(): boolean {
